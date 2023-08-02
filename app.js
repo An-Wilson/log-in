960e66342bb2f5a7f1724a461a7c571a608dbe28
An-Wilson <ta102779@gmail.com>
2023-08-02 18:26:03 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const exphbs = require('express-handlebars')
+const users = [
+  {
+    firstName: 'Tony',
+    email: 'tony@stark.com',
+    password: 'iamironman'
+  },
+  {
+    firstName: 'Steve',
+    email: 'captain@hotmail.com',
+    password: 'icandothisallday'
+  },
+  {
+    firstName: 'Peter',
+    email: 'peter@parker.com',
+    password: 'enajyram'
+  },
+  {
+    firstName: 'Natasha',
+    email: 'natasha@gamil.com',
+    password: '*parol#@$!'
+  },
+  {
+    firstName: 'Nick',
+    email: 'nick@shield.com',
+    password: 'password'
+  }
+]
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(port, () => {
+  console.log(`The Express server is listening on http://localhost:${port}`)
+})
