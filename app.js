b73a5c1b61f3d8843c87d0a4af6f1d2d860dcd46
An-Wilson <ta102779@gmail.com>
2023-08-03 13:55:24 +0800

    feat: add style.css & rename

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000
 
 const exphbs = require('express-handlebars')
+
 const users = [
   {
     firstName: 'Tony',
@@ -34,8 +35,28 @@ const users = [
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
+// setting routes
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('login')
+})
+
+app.post('/', (req, res) => {
+  const email = req.body.email
+  const password = req.body.password
+  if (users.find(data => data.email === email)) {
+    const userIndex = users.findIndex(data => data.email === email)
+    if (users[userIndex].password === password) {
+      const user = users[userIndex]
+      res.render('welcome', { user })
+    } else {
+      res.render('login', { wrongPwMsg: 'Incorrect password.' })
+    }
+  } else {
+    res.render('login', { noAccountMsg: 'Can not found this account.' })
+  }
 })
 
 app.listen(port, () => {
