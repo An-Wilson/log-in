57e65a8eef11863d2022f69a8e72a07d074932a8
An-Wilson <ta102779@gmail.com>
2023-08-04 17:55:23 +0800

    練習：手刻cookie保存機制

--- app.js
+++ app.js
@@ -3,32 +3,38 @@ const app = express()
 const port = 3000
 
 const exphbs = require('express-handlebars')
+const cookieParser = require('cookie-parser')
 
 const users = [
   {
     firstName: 'Tony',
     email: 'tony@stark.com',
-    password: 'iamironman'
+    password: 'iamironman',
+    userId: '001'
   },
   {
     firstName: 'Steve',
     email: 'captain@hotmail.com',
-    password: 'icandothisallday'
+    password: 'icandothisallday',
+    userId: '002'
   },
   {
     firstName: 'Peter',
     email: 'peter@parker.com',
-    password: 'enajyram'
+    password: 'enajyram',
+    userId: '003'
   },
   {
     firstName: 'Natasha',
     email: 'natasha@gamil.com',
-    password: '*parol#@$!'
+    password: '*parol#@$!',
+    userId: '004'
   },
   {
     firstName: 'Nick',
     email: 'nick@shield.com',
-    password: 'password'
+    password: 'password',
+    userId: '005'
   }
 ]
 
@@ -37,6 +43,7 @@ app.set('view engine', 'handlebars')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
+app.use(cookieParser())
 
 // setting routes
 app.get('/', (req, res) => {
@@ -50,13 +57,23 @@ app.post('/', (req, res) => {
     const userIndex = users.findIndex(data => data.email === email)
     if (users[userIndex].password === password) {
       const user = users[userIndex]
-      res.render('welcome', { user })
+      res.cookie('userId', user.userId)
+      return res.render('welcome', { user })
     } else {
       res.render('login', { wrongPwMsg: 'Incorrect password.' })
     }
   } else {
     res.render('login', { noAccountMsg: 'Can not found this account.' })
   }
+})
+
+// 當使用者成功登入後，取得使用者的 cookie 資料，並挑出對應的使用者資料給 profile.handlebars
+app.get('/:id/profile', (req, res) => {
+  const { userId } = req.cookies // 需先安裝 cookie-parser
+  const userIndex = users.findIndex(data => data.userId === userId)
+  const user = users[userIndex]
+  console.log(user) // console.log 檢查是否有找對使用者
+  res.render('profile', { user })
 })
 
 app.listen(port, () => {
